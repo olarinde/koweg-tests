eca922181818f0a8dec8623d75710ba540445718
larinde <olarinde.ajai@gmail.com>
2017-06-27 20:59:57 +0200

    disabled redirection

--- src/test/java/com/koweg/tests/Oauth2Test.java
+++ src/test/java/com/koweg/tests/Oauth2Test.java
@@ -1,6 +1,7 @@
 package com.koweg.tests;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,27 +19,25 @@ public class Oauth2Test {
     private static final String CLIENT_ID = "client_id";
     private static final String STATE = "state";
 
-//    GET 
-//    https://uat1.simulator-api.db.com/gw/oidc/authorize
-//         ?response_type=code&
-//          redirect_uri=<your_redirect_uri>&
-//          client_id=<your_client_id>&
-//          state=<your_state>
 
 
     @Test
     public void oauth2_authorisation_request_test() {
         ValidatableResponse response = RestAssured.given()
-                    .baseUri(BASE_URI)
+                        .baseUri(BASE_URI)
                         .queryParam(RESPONSE_TYPE, "token")
                         .queryParam(REDIRECT_URI, "http://invoice-koweg.rhcloud.com/dailylog/dailylogs")
                         .queryParam(CLIENT_ID, "6381de92-d162-44d3-b949-f5ff8d502566")
                         .queryParam(STATE, "active")
                     .when()
+                         .redirects().follow(false)
                         .get()
                     .then()
-                         .statusCode(is(200));
-        System.out.println(response.extract().asString());
+                         .statusCode(is(302))
+                         .header("Location", is(notNullValue()));
+        System.out.println(response.extract().statusLine());
+        System.out.println(response.extract().header("Location"));
+        System.out.println("\n------- ALL HEADERS -------\n" + response.extract().headers().toString());
     }
 
 }
